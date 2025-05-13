338ad0b5c2ba03534fb66052bdf3af1ef99c1b2f
Diego Santos <diegosantos.engtads@gmail.com>
2025-05-13 16:22:34 -0300

    feat(components): create responsive Container component

---
+++ src/app/components/Container/index.tsx
@@ -0,0 +1,19 @@
+import clsx from 'clsx';
+
+type ContainerProps = {
+  children: React.ReactNode;
+  className?: string;
+};
+
+export function Container({ children, className }: ContainerProps) {
+  return (
+    <div
+      className={clsx(
+        'w-full max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8',
+        className,
+      )}
+    >
+      {children}
+    </div>
+  );
+}
