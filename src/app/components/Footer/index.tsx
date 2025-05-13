0f490f9bddf2ee8709e2fd46ad9ac9f1d4916b5e
Diego Santos <diegosantos.engtads@gmail.com>
2025-05-13 16:23:42 -0300

    feat(components): create Footer with page responsive typography

---
+++ src/app/components/Footer/index.tsx
@@ -0,0 +1,26 @@
+import Link from 'next/link';
+
+export function Footer() {
+  return (
+    <footer className='flex justify-center'>
+      <small className='my-10 text-center px-8'>
+        Criado por{' '}
+        <Link
+          href='https://github.com/diegosantos-engtads'
+          className='inline-block text-[#123] font-medium after:block after:w-0 after:h-[1px] after:bg-[#123] after:transition-all after:duration-400 after:ease-in-out hover:after:w-full'
+          target='_blank'
+        >
+          Diego Santos
+        </Link>{' '}
+        Copyright &copy; {new Date().getFullYear()}{' '}
+        <Link
+          href='/'
+          className='inline-block text-[#123] font-medium after:block after:w-0 after:h-[1px] after:bg-[#123] after:transition-all after:duration-400 after:ease-in-out hover:after:w-full'
+        >
+          Gerenciado de Produtos
+        </Link>{' '}
+        Todos os direitos reservados.
+      </small>
+    </footer>
+  );
+}
