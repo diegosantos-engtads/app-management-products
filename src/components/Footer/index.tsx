43cf45e59365c41fb3eeaf2ce9f8f63515fe6e5b
diegosantos-engtads <diegosantos.engtads@gmail.com>
2025-05-17 19:54:28 -0300

    feat: integrate Footer with structured content and copyright components

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,26 +1,12 @@
-import Link from 'next/link';
+import { CopyrightDiegoSantos } from '../CopyrightDiegoSantos';
+import { ContentFooter } from '../ContentFooter';
 
 export function Footer() {
   return (
-    <footer className='flex justify-center'>
-      <small className='my-10 text-center px-8'>
-        Criado por{' '}
-        <Link
-          href='https://github.com/diegosantos-engtads'
-          className='inline-block text-[#123] font-medium after:block after:w-0 after:h-[1px] after:bg-[#123] after:transition-all after:duration-400 after:ease-in-out hover:after:w-full'
-          target='_blank'
-        >
-          Diego Santos
-        </Link>{' '}
-        Copyright &copy; {new Date().getFullYear()}{' '}
-        <Link
-          href='/'
-          className='inline-block text-[#123] font-medium after:block after:w-0 after:h-[1px] after:bg-[#123] after:transition-all after:duration-400 after:ease-in-out hover:after:w-full'
-        >
-          Gerenciado de Produtos
-        </Link>{' '}
-        Todos os direitos reservados.
-      </small>
+    <footer className='bg-[#151515] text-white mt-20'>
+      <ContentFooter />
+      <hr className='border-solid w-full opacity-8' />
+      <CopyrightDiegoSantos />
     </footer>
   );
 }
