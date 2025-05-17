4e10a92fda7daa6d5a8054cdcbc0f8283b97b7e1
diegosantos-engtads <diegosantos.engtads@gmail.com>
2025-05-17 19:54:24 -0300

    feat: integrate Header with new navigation components

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,22 +1,15 @@
-import clsx from 'clsx';
-import Link from 'next/link';
-import { Container } from '../Container';
+import { NavMenuTop } from '../NavMenuTop';
+import { NavMenu } from '../NavMenu';
+import { NavMenuFilters } from '../NavMenuFilters';
 
 export function Header() {
   return (
-    <header className={clsx(' top-0 flex items-center')}>
-      <Container>
-        <h1
-          className={clsx(
-            'text-2xl/normal font-extrabold py-8',
-            'sm:text-3xl/normal sm:py-10',
-            'md:text-4xl/normal md:py-11',
-            'lg:text-6xl/normal md:py-12',
-          )}
-        >
-          <Link href='/'>Gerenciador de Produtos</Link>
-        </h1>
-      </Container>
+    <header>
+      <NavMenuTop />
+
+      <NavMenu />
+
+      <NavMenuFilters />
     </header>
   );
 }
